2bc878075a29e27813d7f5ce54de0fce1f5cc856
olaniyo <opriolani>
2023-07-27 10:41:37 -0700

    Javascript ES6 Promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getFullResponseAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'success' });
+    reject(Error('The fake API is not working currently'));
+  });
+}
+
+export default getFullResponseAPI;
