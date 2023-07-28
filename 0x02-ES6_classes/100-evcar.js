71f9b9e0ae4bf8d1939062b4c93582aff3b257d8
olaniyo <opriolani>
2023-07-28 00:41:49 -0700

    ES6 Classes

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,18 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super();
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+
+  cloneCar() {
+    const {
+      _brand, _motor, _color, _range,
+    } = this;
+    return new Car(_brand, _motor, _color, _range);
+  }
+}
---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      throw new TypeError('Amount must be a number.');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    } else {
+      throw new TypeError('Currency must be an instance of the Currency class.');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('Amount and conversionRate must be numbers.');
+  }
+}
